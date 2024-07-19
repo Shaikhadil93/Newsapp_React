36fdaa31962e284d3f54e5089d35b2da8a0b68e1
Shaikhadil93 <shaikh87786@gmail.com>
2024-07-19 20:50:27 +0530

    new sample file add and loog the array usin map

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 
 export class NewsItems extends Component {
-    constructor(){
-        super()
-        console.log("i am constructor")
-    }
+   
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
@@ -14,7 +11,7 @@ export class NewsItems extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetails/" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
